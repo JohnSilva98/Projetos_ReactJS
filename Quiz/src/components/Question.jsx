5b6a514cc123dc1fddbf80603f64e5acc2fc4065
JohnSilva98 <jonathan.design16@oulook.com>
2023-11-24 10:00:31 -0300

    Feito Css da pagina de questões

--- Quiz/src/components/Question.jsx
+++ Quiz/src/components/Question.jsx
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import { QuizContext } from "../context/quiz";
+import "./Question.css";
 
 const Question = () => {
   const [quizState, dispatch] = useContext(QuizContext);
