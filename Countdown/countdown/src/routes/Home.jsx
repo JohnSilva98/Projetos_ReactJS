9bab72798a8557836521bd9d88f8de778739a6d4
JohnSilva98 <jonathan.design16@oulook.com>
2023-11-30 15:36:53 -0300

    criação form e css

--- Countdown/countdown/src/routes/Home.jsx
+++ Countdown/countdown/src/routes/Home.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import "./Home.css";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      <h2>Monte sua contagem regressiva!</h2>
+      <form className="countdown-form">
+        <label>
+          <span>Titúlo:</span>
+          <input type="text" name="title" placeholder="Digite o título" />
+        </label>
+        <label>
+          <span>Data do evento:</span>
+          <input type="date" name="date" />
+        </label>
+        <label>
+          <span>Imagem:</span>
+          <input
+            type="text"
+            name="image"
+            placeholder="Insira a url da imagem"
+          />
+        </label>
+        <label>
+          <span>Cor do tema:</span>
+          <input type="color" name="cor" />
+        </label>
+        <input type="submit" value="Enviar"></input>
+      </form>
+    </div>
+  );
 };
 
 export default Home;
