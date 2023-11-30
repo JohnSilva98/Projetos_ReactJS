b4daebd33792dfd736c92ecf6d066d6521ef512c
JohnSilva98 <jonathan.design16@oulook.com>
2023-11-30 12:31:52 -0300

    contador regressivo funcionando

--- Countdown/countdown/src/App.jsx
+++ Countdown/countdown/src/App.jsx
@@ -3,17 +3,19 @@ import Counter from "./components/Counter";
 import newyear from "./assets/newyear.jpg";
 
 import "./App.css";
+import useCountdown from "./hooks/useCountdown";
 
 function App() {
+  const [day, hour, minute, second] = useCountdown("Jan 1, 2024 00:00:00");
   return (
     <div className="App" style={{ backgroundImage: `url(${newyear})` }}>
       <div className="container">
         <Title title="Contagem regressiva para 2024" />
         <div className="countdown-container">
-          <Counter title="Dias" number={2} />
-          <Counter title="Horas" number={2} />
-          <Counter title="Minutos" number={2} />
-          <Counter title="Segundos" number={2} />
+          <Counter title="Dias" number={day} />
+          <Counter title="Horas" number={hour} />
+          <Counter title="Minutos" number={minute} />
+          <Counter title="Segundos" number={second} />
         </div>
       </div>
     </div>
