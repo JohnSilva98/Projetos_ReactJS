09b0efce0ef3330022117a794326145c9a39588b
JohnSilva98 <jonathan.design16@oulook.com>
2023-12-14 17:07:56 -0300

    Criado requisição

--- Party_Time/backend/controllers/serviceController.js
+++ Party_Time/backend/controllers/serviceController.js
@@ -1,5 +1,22 @@
 const { Service: ServiceModel } = require("../models/Service");
 
-const serviceController = {};
+const serviceController = {
+  create: async (req, res) => {
+    try {
+      const service = {
+        name: req.body.name,
+        description: req.body.description,
+        price: req.body.price,
+        image: req.body.image,
+      };
+
+      const response = await ServiceModel.create(service);
+
+      res.status(201).json({ response, msg: "serviço criado com sucesso!" });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+};
 
 module.exports = serviceController;
--- Party_Time/backend/routes/services.js
+++ Party_Time/backend/routes/services.js
@@ -3,5 +3,7 @@ const router = require("express").Router();
 const serviceController = require("../controllers/serviceController");
 
 // Funções
-
+router
+  .route("/services")
+  .post((req, res) => serviceController.create(req, res));
 module.exports = router;
