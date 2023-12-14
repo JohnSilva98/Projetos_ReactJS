a9e3b1389f80bf13f76be24145e75f0fceb00146
JohnSilva98 <jonathan.design16@oulook.com>
2023-12-14 17:49:03 -0300

    criado função de fazer get de um dado individual por id, com verificação se o id existe ou nao

--- Party_Time/backend/controllers/serviceController.js
+++ Party_Time/backend/controllers/serviceController.js
@@ -26,6 +26,21 @@ const serviceController = {
       console.log(error);
     }
   },
+  get: async (req, res) => {
+    try {
+      const id = req.params.id;
+      const service = await ServiceModel.findById(id);
+
+      if (!service) {
+        res.status(404).json({ msg: "serviço não encontrado." });
+        return;
+      }
+
+      res.json(service);
+    } catch (error) {
+      console.log(error);
+    }
+  },
 };
 
 module.exports = serviceController;
--- Party_Time/backend/routes/services.js
+++ Party_Time/backend/routes/services.js
@@ -8,4 +8,8 @@ router
   .post((req, res) => serviceController.create(req, res));
 
 router.route("/services").get((req, res) => serviceController.getAll(req, res));
+
+router
+  .route("/services/:id")
+  .get((req, res) => serviceController.get(req, res));
 module.exports = router;
