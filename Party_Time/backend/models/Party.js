c720aea0fd16ccd50dc7e282e67f495d83c99e53
JohnSilva98 <jonathan.design16@oulook.com>
2023-12-14 16:08:24 -0300

    criado models

---
+++ Party_Time/backend/models/Party.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const { serviceSchema } = require("./Service");
+
+const partySchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    budget: {
+      type: Number,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    services: {
+      type: [serviceSchema],
+    },
+  },
+  { timestamps: true }
+);
+
+const Party = mongoose.model("Party", partySchema);
+
+module.exports = partySchema;
