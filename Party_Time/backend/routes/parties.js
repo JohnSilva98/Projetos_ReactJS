a7856a6eae297b1b9815b512b587626255d7447e
JohnSilva98 <jonathan.design16@oulook.com>
2024-01-18 17:22:34 -0300

    .

--- Party_Time/backend/routes/parties.js
+++ Party_Time/backend/routes/parties.js
@@ -2,4 +2,6 @@ const router = require("express").Router();
 
 const partyController = require("../controllers/serviceController");
 
+router.route("/parties").post((req, res) => partyController.create(req, res));
+
 module.exports = router;
