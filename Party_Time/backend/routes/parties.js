fb78cb3feb00f38921a55eb40747ca65d9c346c9
JohnSilva98 <jonathan.design16@oulook.com>
2024-01-19 12:32:23 -0300

    Update the party controller to use the party controller instead of the service controller. (generated by blackbox.ai )

--- Party_Time/backend/routes/parties.js
+++ Party_Time/backend/routes/parties.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 
-const partyController = require("../controllers/serviceController");
+const partyController = require("../controllers/partyController");
 
 router.route("/parties").post((req, res) => partyController.create(req, res));
 
