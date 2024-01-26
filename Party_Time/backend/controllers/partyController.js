dcde106f14c1fca0570837ad215276ce4eb06b58
JohnSilva98 <jonathan.design16@oulook.com>
2024-01-26 11:25:22 -0300

    finishing all api reqs

--- Party_Time/backend/controllers/partyController.js
+++ Party_Time/backend/controllers/partyController.js
@@ -64,6 +64,34 @@ const partyController = {
     const deletedParty = await PartyModel.findByIdAndDelete(id);
     res.status(200).json({ deletedParty, msg: "Festa deletada com sucesso!" });
   },
+  update: async (req, res) => {
+    try {
+      const id = req.params.id;
+
+      const party = {
+        title: req.body.title,
+        author: req.body.author,
+        description: req.body.description,
+        budget: req.body.budget,
+        image: req.body.image,
+        services: req.body.services,
+      };
+
+      if (party.services && !checkPartyBudget(party.budget, party.services)) {
+        res.status(406).json({ msg: "O seu orçamento é insuficiente!" });
+        return;
+      }
+
+      const updatedParty = await PartyModel.findByIdAndUpdate(id, party);
+      if (!updatedParty) {
+        res.status(404).json({ msg: "A festa não foi encontrada" });
+        return;
+      }
+      res.status(200).json({ party, msg: "Festa atualizada com sucesso!" });
+    } catch (error) {
+      console.log(error);
+    }
+  },
 };
 
 module.exports = partyController;
