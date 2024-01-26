489245b52b154a76ab5161dca4bfbe0e1b089942
JohnSilva98 <jonathan.design16@oulook.com>
2024-01-26 11:43:33 -0300

    creating initial routes for home and createparty pages

--- Party_Time/frontend/src/App.jsx
+++ Party_Time/frontend/src/App.jsx
@@ -1,9 +1,11 @@
+import { Outlet } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Party Time</h1>
+      <Outlet />
     </div>
   );
 }
--- Party_Time/frontend/src/main.jsx
+++ Party_Time/frontend/src/main.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Home from "./routes/Home";
+import CreateParty from "./routes/CreateParty.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/party/new",
+        element: <CreateParty />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
