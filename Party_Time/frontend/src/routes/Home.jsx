eb9dc5248cd47b7380ed86f163a59bde79208f71
JohnSilva98 <jonathan.design16@oulook.com>
2024-01-26 14:47:48 -0300

    getting data from the parties and showing up

--- Party_Time/frontend/src/routes/Home.jsx
+++ Party_Time/frontend/src/routes/Home.jsx
@@ -1,7 +1,41 @@
 import React from "react";
+import partyFetch from "../axios/config";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [parties, setParties] = useState(null);
+
+  //   load parties
+  useEffect(() => {
+    const loadParties = async () => {
+      const res = await partyFetch.get("/parties");
+      console.log(res);
+      setParties(res.data);
+    };
+
+    loadParties();
+  }, []);
+
+  if (!parties) return <p>Carregando...</p>;
+
+  return (
+    <div className="home">
+      <h1>Suas festas</h1>
+      <div className="parties-container">
+        {parties.length === 0 && <p>Não há festas cadastradas!</p>}
+        {parties.map((party) => (
+          <div className="party" key={party._id}>
+            <img src={party.image} alt={party.title} />
+            <h3>{party.title}</h3>
+            <Link to={`/party/${party._id}`} className="btn-secondary">
+              Detalhes
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
