6bddde4d8ce9c897a44eb467a6ab2bdf47157052
JohnSilva98 <jonathan.design16@oulook.com>
2024-02-20 14:29:16 -0300

    creating multer for images

---
+++ memories/helpers/upload.js
@@ -0,0 +1,25 @@
+const multer = require("multer");
+const path = require("path");
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, "../public/images"));
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+
+const filterFilter = (req, file, cb) => {
+  if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+const upload = multer({
+  storage,
+  filterFilter,
+});
+
+module.exports = upload;
