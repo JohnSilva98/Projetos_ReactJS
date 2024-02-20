fd79b2d7b556a5ab7b6a914fae373bf2852073eb
JohnSilva98 <jonathan.design16@oulook.com>
2024-02-20 12:36:26 -0300

    inicial setup

--- memories/backend/app.js
+++ memories/backend/app.js
@@ -4,3 +4,8 @@ const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
+const port = 3000;
+app.listen(port, async () => {
+  console.log(`O servidor iniciou na porta: ${port}`);
+});
