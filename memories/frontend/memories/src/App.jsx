d83b3697530b28d3229eec6c5699100f34ed33ea
JohnSilva98 <jonathan.design16@oulook.com>
2024-03-04 16:04:47 -0300

    react router setup

--- memories/frontend/memories/src/App.jsx
+++ memories/frontend/memories/src/App.jsx
@@ -1,9 +1,12 @@
 import "./App.css";
-
+import { Outlet } from "react-router-dom";
 function App() {
   return (
     <div className="app">
       <h1>memories</h1>
+      <div className="container">
+        <Outlet />
+      </div>
     </div>
   );
 }
---
+++ memories/frontend/memories/src/routes/addMemory.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const addMemory = () => {
+  return <div>addMemory</div>;
+};
+
+export default addMemory;
