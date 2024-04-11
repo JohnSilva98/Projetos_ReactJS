e640c8d31eb7dea70fde6a8b7921c77aa302bb40
JohnSilva98 <jonathan.design16@oulook.com>
2024-04-11 11:31:40 -0300

    bringing data to home

--- memories/frontend/memories/src/routes/Home.jsx
+++ memories/frontend/memories/src/routes/Home.jsx
@@ -1,7 +1,26 @@
 import React from "react";
-
+import axios from "../axios-config";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 const Home = () => {
-  return <div>Home</div>;
+  const [memories, setMemories] = useState([]);
+
+  useEffect(() => {
+    const getMemories = async () => {
+      const res = await axios.get("/memories");
+      setMemories(res.data);
+    };
+    getMemories();
+  }, []);
+
+  return (
+    <div className="home">
+      <h2>Confira as últimas memórias</h2>
+      <div className="memories-container">
+        {memories.length > 0 && memories.map((memory) => <p>{memory.title}</p>)}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
