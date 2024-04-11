4d35d52e84531cccba9796a8fa18b332acd8466f
JohnSilva98 <jonathan.design16@oulook.com>
2024-04-11 11:25:16 -0300

    creating toastify notifications and redirecting to home

--- memories/frontend/memories/src/App.jsx
+++ memories/frontend/memories/src/App.jsx
@@ -3,9 +3,23 @@ import { Outlet } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 function App() {
   return (
     <div className="app">
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
       <Navbar />
       <div className="container">
         <Outlet />
--- memories/frontend/memories/src/routes/addMemory.jsx
+++ memories/frontend/memories/src/routes/addMemory.jsx
@@ -1,26 +1,64 @@
 import React from "react";
-import axios from "axios";
+import axios from "../axios-config";
 import { useState } from "react";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 import "./AddMemory.css";
 const addMemory = () => {
+  const [inputs, setInputs] = useState({});
+  const [image, setImage] = useState(null);
+  const navigate = useNavigate();
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const formdata = new FormData();
+    formdata.append("image", image);
+    formdata.append("title", inputs.title);
+    formdata.append("description", inputs.description);
+
+    try {
+      const response = await axios.post("/memories", formdata, {
+        headers: { "Content-Type": "multipart/form-data" },
+      });
+      toast.success(response.data.msg);
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+      toast.error(error.response.data.msg);
+    }
+  };
+
+  const handleChange = (event) => {
+    if (event.target.name === "image") {
+      setImage(event.target.files[0]);
+    } else {
+      setInputs({ ...inputs, [event.target.name]: event.target.value });
+    }
+  };
   return (
     <div className="add-memory-page">
       <h2>Crie uma nova memória</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>
           <p>Título</p>
-          <input type="text" placeholder="Defina um título" name="title" />
+          <input
+            type="text"
+            placeholder="Defina um título"
+            name="title"
+            onChange={handleChange}
+          />
         </label>
         <label>
           <p>Descrição</p>
           <textarea
             name="description"
             placeholder="Explique o que aconteceu"
+            onChange={handleChange}
           ></textarea>
         </label>
         <label>
           <p>Foto:</p>
-          <input type="file" name="image" />
+          <input type="file" name="image" onChange={handleChange} />
         </label>
         <input type="submit" value="Enviar" className="btn" />
       </form>
