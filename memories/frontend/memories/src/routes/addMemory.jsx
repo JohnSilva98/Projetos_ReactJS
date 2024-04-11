0b8a112645298a14cfa752081d4bb1bee50e794f
JohnSilva98 <jonathan.design16@oulook.com>
2024-04-11 10:25:30 -0300

    finishing addmemory css

--- memories/frontend/memories/src/routes/addMemory.jsx
+++ memories/frontend/memories/src/routes/addMemory.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import { useState } from "react";
+import "./AddMemory.css";
 const addMemory = () => {
   return (
     <div className="add-memory-page">
